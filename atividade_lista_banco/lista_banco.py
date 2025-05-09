76f0cffefc4f9de094b3ca6d6928b8f6dc4faeae
GabeBersani <gabi-fernanda1@outlook.com>
2025-05-09 09:32:25 -0300

    lista

--- atividade_lista_banco/lista_banco.py
+++ atividade_lista_banco/lista_banco.py
@@ -1,83 +1,102 @@
 import flet as ft
-from flet import AppBar, Text, View
+from flet import AppBar, Text, View, ElevatedButton
 from flet.core.colors import Colors
 
-class User():
-    def __init__(self, titulo, autor):
-        self.titulo = titulo
+class Livro():
+    def __init__(self, nome, descricao, categoria, autor):
+        self.nome = nome
+        self.descricao = descricao
+        self.categoria = categoria
         self.autor = autor
 
 def main(page: ft.Page):
     # Configurações
     page.title = "Exemplo de Rotas"
-    page.theme_mode = ft.ThemeMode.DARK  # ou ft.ThemeMode.DARK
+    page.theme_mode = ft.ThemeMode.LIGHT  # ou ft.ThemeMode.DARK
     page.window.width = 375
     page.window.height = 667
 
-    # Funções
-    lista = []
+    livros = []
 
-    def salvar_nome(e):
-        if titulo_livro.value == "":
-            page.overlay.append(msg_erro)
+    def salvar_livro(e):
+        if input_nome.value.strip() == "":
             msg_erro.open = True
-            page.update()
         else:
-            obj_user = User(
-                titulo=titulo_livro.value,
-                autor=autor_livro.value,
+            livro = Livro(
+                input_nome.value,
+                input_descricao.value,
+                input_categoria.value,
+                input_autor.value
             )
-            lista.append(obj_user)
-            titulo_livro.value = ""
-            page.overlay.append(msg_sucesso)
+            livros.append(livro)
+
+            input_nome.value = ""
+            input_descricao.value = ""
+            input_categoria.value = ""
+            input_autor.value = ""
             msg_sucesso.open = True
-            page.update()
 
-
+        page.update()
 
     def exibir_lista(e):
-        lv_nome.controls.clear()
-        for user in lista:
-            lv_nome.controls.append(
+        lista_livros.controls.clear()
+        for i, livro in enumerate(livros):
+            lista_livros.controls.append(
                 ft.Card(
                     content=ft.Container(
                         content=ft.Column([
-                            Text(f"Titulo: {user.titulo}"),
-                            Text(f"Autor: {user.autor}"),
-                        ])
+                            Text(f" Nome: {livro.nome}", size=16),
+                            Text(f" Autor: {livro.autor}"),
+                            ft.Row([
+                                ElevatedButton("Visualizar Detalhes", on_click=lambda e, idx=i: ver_detalhes(idx))
+                            ])
+                        ]),
+                        padding=10
                     )
                 )
             )
         page.update()
 
-    def gerencia_rotas(e):
+    def ver_detalhes(index):
+        livro = livros[index]
+        page.views.append(
+            View(
+                "/detalhes",
+                [
+                    AppBar(title=Text("Detalhes do Livro"), bgcolor=Colors.TERTIARY_CONTAINER),
+                    Text(f" Descrição: {livro.descricao}", size=18),
+                    Text(f" Categoria: {livro.categoria}", size=18),
+                    ElevatedButton(text="Voltar", on_click=voltar)
+                ]
+            )
+        )
+        page.update()
+
+    def gerencia_rota(e):
         page.views.clear()
         page.views.append(
             View(
                 "/",
                 [
-                    AppBar(title=Text("Home"), bgcolor=Colors.PRIMARY_CONTAINER),
-                    titulo_livro,
-                    autor_livro,
-                    ft.Button(text="Salvar",
-                              on_click=lambda _: salvar_nome(e)
-                              ),
-                    ft.Button(text="Exibir",
-                              on_click=lambda _: page.go("/segunda"))
-                ],
+                    AppBar(title=Text("Cadastro de Livros"), bgcolor=Colors.PRIMARY_CONTAINER),
+                    input_nome,
+                    input_descricao,
+                    input_categoria,
+                    input_autor,
+                    ElevatedButton(text="Salvar Livro", on_click=salvar_livro),
+                    ElevatedButton(text="Exibir Lista", on_click=lambda _: page.go("/lista")),
+                ]
             )
         )
-
-        if page.route == "/segunda":
+        if page.route == "/lista":
             exibir_lista(e)
             page.views.append(
                 View(
-                    "/segunda",
+                    "/lista",
                     [
-                        AppBar(title=Text("Segunda tela"), bgcolor=Colors.SECONDARY_CONTAINER),
-                        lv_nome,
-
-                    ],
+                        AppBar(title=Text("Lista de Livros"), bgcolor=Colors.SECONDARY_CONTAINER),
+                        lista_livros
+                    ]
                 )
             )
         page.update()
@@ -87,30 +106,23 @@ def main(page: ft.Page):
         top_view = page.views[-1]
         page.go(top_view.route)
 
-    # Componentes
-    titulo_livro = ft.TextField(label="Titulo", hint_text="Digite o Titulo do livro")
-    autor_livro = ft.TextField(label="Autor", hint_text="Digite a autor do livro")
+    # Campos
+    input_nome = ft.TextField(label="Nome do Livro")
+    input_descricao = ft.TextField(label="Descrição")
+    input_categoria = ft.TextField(label="Categoria")
+    input_autor = ft.TextField(label="Autor")
 
-    msg_sucesso = ft.SnackBar(
-        content=ft.Text("Salvo com sucesso"),
-        bgcolor=Colors.GREEN
-    )
+    lista_livros = ft.Column()
 
-    msg_erro = ft.SnackBar(
-        content=ft.Text("Erro, esta vazio"),
-        bgcolor=Colors.RED
-    )
+    msg_sucesso = ft.SnackBar(content=Text("Livro cadastrado!"), bgcolor=Colors.GREEN)
+    msg_erro = ft.SnackBar(content=Text("Não deixe campos vazio"), bgcolor=Colors.RED)
 
-    lv_nome = ft.ListView(
-        height=500
-    )
-
-    # Eventos
-    page.on_route_change = gerencia_rotas
+    page.overlay.append(msg_sucesso)
+    page.overlay.append(msg_erro)
+    page.on_route_change = gerencia_rota
     page.on_view_pop = voltar
 
     page.go(page.route)
-
 
 # Comando que executa o aplicativo
 # Deve estar sempre colado na linha
